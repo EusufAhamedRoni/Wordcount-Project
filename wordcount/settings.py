dc7c8f505e5f1c06f480980c1204a3d4580d7eeb
Eusuf Ahamed <eusufahmed789@gmail.com>
2020-08-18 09:32:00 +0600

    Working on version 1.1

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's!ju7_d43-d(hckpcjtyon*=isu+!8+2#t-$x*r4!gd+@-!xcv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
